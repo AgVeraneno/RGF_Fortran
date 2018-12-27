0f7acd26b6ecedf1cbbac0c0bf25b8893bdcaeab
Alex <30921alex@gmail.com>
2018-12-27 21:56:26 +0800

    加入專案檔案。

---
+++ RGF_Fortran/RGF_Fortran.f90
@@ -0,0 +1,25 @@
+!  RGF_Fortran.f90 
+!
+!  FUNCTIONS:
+!  RGF_Fortran - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: RGF_Fortran
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program RGF_Fortran
+
+    implicit none
+
+    ! Variables
+
+    ! Body of RGF_Fortran
+    print *, 'Hello World'
+
+    end program RGF_Fortran
+
